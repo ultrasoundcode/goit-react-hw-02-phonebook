ef74e2bfead6afa3ea041d14a20fb9d23d1e5075
askhat <0x.ultrasound@gmail.com>
2023-05-08 15:02:53 +0600

    Update
    add packages and update files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import { GlobalStyle } from 'components/GlobalStyle';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
